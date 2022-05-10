a339b123faf9a46061a433aefca5e4789f961a80
broklyn777 <50321354+broklyn777@users.noreply.github.com>
2022-05-10 10:58:56 +0200

    1st 15:03

---
+++ components/Navbar.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+import styles from "../styles/Navbar.module.css"
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <Link href="/">Media3D</Link>
+      
+      </div>
+  )
+}
+
+export default Navbar
